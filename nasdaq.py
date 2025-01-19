d9d9190167ceec49573f7ef14ae1019e74696d40
microslaw <milosz.grunwald@gmail.com>
2025-01-19 22:11:02 +0100

    add yfinance and adapt nsdaq loading

--- nasdaq.py
+++ nasdaq.py
@@ -3,24 +3,35 @@ import pandas as pd
 
 ## nasdaq list: https://www.slickcharts.com/nasdaq100
 
-nasdaq_top_10 = ["AAPL", "NVDA", "MSFT", "AMZN", "AVGO", "TSLA", "META", "GOOGL", "GOOG", "COST"]
 
-start_date = "2021-01-01"
-end_date = "2021-12-31"
+def load_nasdaq_data():
+    nasdaq_top_10 = [
+        "AAPL",
+        "NVDA",
+        "MSFT",
+        "AMZN",
+        "AVGO",
+        "TSLA",
+        "META",
+        "GOOGL",
+        "GOOG",
+        "COST",
+    ]
 
-all_stock_data = pd.DataFrame()
+    start_date = "2021-01-01"
+    end_date = "2021-12-31"
 
-for ticker in nasdaq_top_10:
-    stock_data = yf.download(ticker, start=start_date, end=end_date)
-    stock_data.reset_index(inplace=True)
-    stock_data = stock_data[["Date", "Open", "High", "Low", "Close", "Volume"]]
-    stock_data.columns = ["date", "open", "high", "low", "close", "volume"] 
-    stock_data["company"] = ticker  
-    all_stock_data = pd.concat([all_stock_data, stock_data], ignore_index=True)
+    all_stock_data = pd.DataFrame()
 
-    all_stock_data[["open", "high", "low", "close", "volume"]] = all_stock_data[["open", "high", "low", "close", "volume"]].round(2)
+    for ticker in nasdaq_top_10:
+        stock_data = yf.download(ticker, start=start_date, end=end_date)
+        stock_data.reset_index(inplace=True)
+        stock_data = stock_data[["Date", "Open", "High", "Low", "Close", "Volume"]]
+        stock_data.columns = ["date", "open", "high", "low", "close", "volume"]
+        stock_data["company"] = ticker
+        all_stock_data = pd.concat([all_stock_data, stock_data], ignore_index=True)
 
-
-all_stock_data.to_csv("nasdaq_top_10_stock_data_2021.csv", index=False)
-
-
+    all_stock_data[["open", "high", "low", "close", "volume"]] = all_stock_data[
+        ["open", "high", "low", "close", "volume"]
+    ].round(2)
+    all_stock_data.to_csv("nasdaq_top_10_stock_data_2021.csv", index=False)
